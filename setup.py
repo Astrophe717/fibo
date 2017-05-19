a284403a90e1387b656a61369c5927df2184d530
Astrophe717 <celyn24@hotmail.com>
2017-05-19 12:24:57 +0100

    Change version to 0.1dev as per python docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fibo',
-    version='0.1',
+    version='0.1dev',
     packages=['fibo'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
