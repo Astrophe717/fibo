994cf682aeb4266bf7f7d1f1a59070636aca5328
Astrophe717 <celyn24@hotmail.com>
2017-05-19 12:08:54 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='fibo',
+    version='0.1',
+    packages=['fibo'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
